cedd0e39396ab9a9dca0c482f55e54225c563991
Lubna Khalid <lkhalid0095@bths.edu>
2022-01-21 15:30:31 -0500

    Created campus table. Closes issue#1

--- server/api/index.js
+++ server/api/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
 
 router.use("/students", require("./student"))
-
+router.use("/campus", require("./campus"))
 module.exports = router
--- server/db/campus.js
+++ server/db/campus.js
@@ -1,1 +1,21 @@
-// needs to be done
+const Sequelize = require('sequelize')
+const db = require("./database")
+
+const Campus = db.define('campus', {
+    name: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    image: {
+        type: Sequelize.TEXT,
+    },
+    address: {
+        type: Sequelize.TEXT,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING(4000),
+    }
+})
+
+module.exports = Campus

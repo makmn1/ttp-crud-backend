af2b95d0bc94665a4a1a200011141707c2d02e2c
Mohamed Mohamed <therealmohamedmohamed@gmail.com>
2022-01-21 19:55:16 -0500

    Defined relationships between campus and student models

--- server/db/index.js
+++ server/db/index.js
@@ -2,6 +2,12 @@ const db = require("./database")
 const Student = require("./student")
 const Campus = require("./campus")
 
+
+Campus.belongsToMany(Student, {through: "Campus_Student"})
+
+Campus.hasMany(Student)
+Student.belongsTo(Campus)
+
 module.exports = {
     db,
     Student,
